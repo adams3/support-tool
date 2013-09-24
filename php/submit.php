37de09e4c92fce0fd2ed806655805c403fca83d7
adams3 <Adams@Adams-MacBook-Pro-2.local>
2013-09-24 22:58:31 +0200

    start of login

--- php/submit.php
+++ php/submit.php
@@ -5,7 +5,6 @@ require_once dirname(__FILE__) . '/vendor/autoload.php';
 
 use Mailgun\Mailgun;
 
-$to = "jkdubr@me.com";
 $to = "a.studenic@gmail..com";
 $subject = "support";
 $from = $_GET["mail"];
